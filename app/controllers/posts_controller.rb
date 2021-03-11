8a0600ed80493130920dfc1a69860eea8feb7e33
YusukeNaito08 <yusuke0208na@gmail.com>
2021-03-11 16:16:39 +0000

    Add: Post/edit update delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,12 +19,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_parms)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
   end
 
 private
